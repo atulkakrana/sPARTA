f6b44c4cf05cc6e712c40ce9061e86b085e5055e
Reza Hammond <rkweku@udel.edu>
2016-05-17 22:42:06 -0500

    Updated version number in sPARTA.py

--- sparta/sPARTA.py
+++ sparta/sPARTA.py
@@ -1,5 +1,5 @@
 ## sPARTA: small RNA-PARE Target Analyzer public version 
-## Updated: version-1.13 11/24/2015
+## Updated: version-1.14 04/25/2016
 ## Property of Meyers Lab at University of Delaware
 ## Author: kakrana@udel.edu
 
